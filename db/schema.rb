b7b831dd330f8a91dbe2290ee95a2384e7809789
Merline Belizaire <merline@noukod.com>
2019-01-17 20:24:30 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20190117180905) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "balance"
-    t.string "password"
-  end
-
 end
