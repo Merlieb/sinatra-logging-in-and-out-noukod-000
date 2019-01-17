ff1ea6e8fda1362cda1912a41de8dc4bafe41128
Merline Belizaire <merline@noukod.com>
2019-01-17 20:27:12 +0000

    Done.

--- db/migrate/20190117180905_create_users.rb
+++ db/migrate/20190117180905_create_users.rb
@@ -2,8 +2,8 @@ class CreateUser < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :balance
       t.string :password
+        t.decimal :balance
     end
   end
 end
